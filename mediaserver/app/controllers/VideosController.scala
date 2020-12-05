88f6d04df37b4362339f987d5f101d9bfb4b3b17
naotaka <>
2020-12-06 02:48:02 +0900

    7章JWTの複合まで完了

--- mediaserver/app/controllers/VideosController.scala
+++ mediaserver/app/controllers/VideosController.scala
@@ -1,26 +1,17 @@
 package controllers
 
-import java.nio.file.{FileSystems, Files, StandardCopyOption}
-import java.time.{Clock, LocalDateTime}
-import java.util.UUID
 import javax.inject._
-
-import domain.entity.{Video, VideoStatus}
-import domain.repository.VideoRepository
 import play.api.Configuration
 import play.api.mvc._
 import play.api.libs.json.Json
-import play.Logger
+
 import pdi.jwt.{Jwt, JwtAlgorithm}
 
 import scala.util.Success
 import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class VideosController @Inject()(cc: ControllerComponents,
-                                 videoRepository: VideoRepository,
-                                 clock: Clock,
                                  configuration: Configuration) extends AbstractController(cc) {
 
   val secret = configuration.get[String]("nnDouga.secret")
@@ -38,28 +29,7 @@ class VideosController @Inject()(cc: ControllerComponents,
                 val json = Json.parse(jsonString)
                 val userId = (json \ "userId").validate[Long].get
                 val expire = (json \ "expire").validate[Long].get
-
-                if (System.currentTimeMillis() / 1000 <= expire) {
-                  val videoId = UUID.randomUUID().toString
-                  val originalFilepath = FileSystems.getDefault.getPath(originalStoreDirPath, videoId)
-                  Files.copy(file.ref.path, originalFilepath, StandardCopyOption.COPY_ATTRIBUTES)
-                  Logger.debug(s"File posted : ${originalFilepath}")
-                  Future.successful(Ok(s"File stored."))
-                  val now = LocalDateTime.now(clock)
-                  val video = Video(
-                    videoId,
-                    ct,
-                    userId,
-                    VideoStatus.OriginalFileSubmitted,
-                    now,
-                    now
-                  )
-                  val future = videoRepository.create(video)
-                  future.map(_ => Ok("Video stored."))
-                } else {
-                  Future.successful(BadRequest("Api token expired."))
-                }
-              case _ => Future.successful(BadRequest("Need file and api token data."))
+                Future.successful(Ok(s"userId:${userId}, expire:${expire}"))
             }
         }
       case _ => Future.successful(BadRequest("Need form data."))
--- mediaserver/build.sbt
+++ mediaserver/build.sbt
@@ -1,5 +1,5 @@
 name := """mediaserver"""
-organization := "nico.ed.nn"
+organization := "nico.ed.nnn"
 
 version := "1.0-SNAPSHOT"
 
@@ -13,17 +13,8 @@ libraryDependencies ++= Seq(
   "com.pauldijou" %% "jwt-core" % "4.2.0"
 )
 
-libraryDependencies += jdbc
-libraryDependencies ++= Seq(
-  "com.h2database"  %  "h2"                           % "1.4.200", // your jdbc driver here
-  "org.scalikejdbc" %% "scalikejdbc"                  % "3.5.0",
-  "org.scalikejdbc" %% "scalikejdbc-config"           % "3.5.0",
-  "org.scalikejdbc" %% "scalikejdbc-play-initializer" % "2.8.0-scalikejdbc-3.5"
-)
-libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.36"
-
 // Adds additional packages into Twirl
-//TwirlKeys.templateImports += "nico.ed.nn.controllers._"
+//TwirlKeys.templateImports += "nico.ed.nnn.controllers._"
 
 // Adds additional packages into conf/routes
-// play.sbt.routes.RoutesKeys.routesImport += "nico.ed.nn.binders._"
+// play.sbt.routes.RoutesKeys.routesImport += "nico.ed.nnn.binders._"
